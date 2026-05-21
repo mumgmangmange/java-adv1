270b2e3c4efcd1944a9f2b9c6eb7efceb1462aa3
mumgmangmange <mumgmangmange@gmail.com>
2026-05-21 14:40:26 +0900

    volatile 타입 학습

---
+++ src/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,33 @@
+package thread.volatile1;
+
+import static thread.start.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+    }
+
+    static class MyTask implements Runnable {
+        volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task 시작");
+            while (runFlag) {
+                //log("ㅠㅣ뮤ㅣ뮤ㅣㅁ");
+            }
+            log("task 종료");
+        }
+    }
+}
